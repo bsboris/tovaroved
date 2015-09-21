5b131d1ff3461120732b927d2ef190a8ab31eee0
bsboris <bsboris@gmail.com>
2015-09-21 17:57:38 +0300

    Fix assortment association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :brand
   belongs_to :category
-  has_many :assortments
+  has_many :assortments, dependent: :destroy
   has_many :stores, through: :assortments
 
   validates :brand, :category, :name, presence: true
